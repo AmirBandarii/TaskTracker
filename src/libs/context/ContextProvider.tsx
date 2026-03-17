c248ccd19e431a7f6f752d50fd6d52eac66cb364
AmirBandarii <amirbandari152@gmail.com>
2026-03-17 16:09:56 +0400

    fixed try catch

--- src/libs/context/ContextProvider.tsx
+++ src/libs/context/ContextProvider.tsx
@@ -5,22 +5,18 @@ import { type ITodo } from '../types/todo'
 
 const ContextProvider: React.FC<Props> = ({ children }) => {
   const [modalShow, setModalShow] = useState<boolean | undefined>(false)
-
-  // 1. This should just be for the 'Create' input field (an empty string)
   const [todo, setTodo] = useState<string | undefined>('')
   const [description, setDescription] = useState<string>('')
-  // 2. MOVE the localStorage logic HERE to initialize your list
   const [todos, setTodos] = useState<ITodo[]>(() => {
-    const saved = localStorage.getItem('todo-app-data')
-    // Check if data exists and is a valid array
-    if (saved != null) {
-      try {
-        return JSON.parse(saved)
-      } catch (e) {
-        return []
-      }
+    const saved = localStorage.getItem('todo-app-data');
+
+    if (saved == null) return []
+
+    try {
+      return JSON.parse(saved);
+    } catch {
+      return []
     }
-    return []
   })
 
   const [isEdit, setIsEdit] = React.useState<Record<string, boolean>>({})
